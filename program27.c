0e7449f8fec63b91dfa5d2de6cec2208ee3eb1e0
Shreya-Pramod-Pasalkar <shreyappasalkar04@gmail.com>
2025-10-30 21:51:29 +0530

    Initial Commit

---
+++ program27.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+//Dynamic Function
+
+void Display(int iFrequency)
+{
+   int iCnt = 0;
+
+   if(iFrequency < 0)
+   {
+    printf("Invalid input\n");
+    return;
+   }
+
+   for(iCnt = 1; iCnt <= iFrequency; iCnt++)
+   {
+    printf("Jay Ganesh...");
+   }
+}
+
+int main()
+{
+    int iCount = 0;
+    printf("Enter the frequency :\n");
+    scanf("%d",&iCount);
+
+    Display(iCount); 
+    
+    return 0;
+}
---
+++ program3.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class program3
+{
+    public static void main(String A[])
+    {
+        System.out.println("Jay Ganesh...");
+    }
+}
